224c04aaad90be114e07ae78e80e43ab1517bd62
Flexander <akran91@gmail.com>
2020-09-10 21:29:14 +0100

    Removes master css file

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,11 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var cleanCSS = require('gulp-clean-css');
 
 function css() {
-    return gulp.src(['./src/scss/**/*.scss', '!./src/css/**/master.css'])
+    return gulp.src(['./src/scss/**/!(master)*.scss'])
         .pipe(sass())
+        .pipe(cleanCSS())
         .pipe(gulp.dest('./dist/css'));
 }
 
@@ -12,3 +14,5 @@ gulp.task('css', css);
 gulp.task('watch', function() {
     return gulp.watch('./src/scss/**/*.scss', css);
 });
+
+gulp.task('default', gulp.series(['css', 'watch']));
